0253f2c09ab089410cd68c94f746b62cebe8b7df
Rogerio Medeiros <argerim@gmail.com>
2012-06-25 00:09:26 -0300

    quiz usado na aula de tv digital na cajuina.

---
+++ scripts/quests.lua
@@ -0,0 +1,17 @@
+quests = {
+	{
+		quest = "O coxinha é gay?",
+	  	resposta = {"sim", "não", "pode ser"},
+	  	correta = 1
+	},
+	{
+	  	quest = "O gil é gay?",
+	  	resposta = {"sim", "não", "pode ser"},
+	  	correta = 1
+	},
+	{
+		quest = "O pavao é gay?",
+	  	resposta = {"sim", "não", "pode ser"},
+		correta = 3
+	}	
+}
